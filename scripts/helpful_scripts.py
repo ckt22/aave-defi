63987171636f363bbd74757911ff0491f8ac2cf7
ckt22 <tangchekkit@kidpairing.com>
2022-03-17 15:14:31 +0800

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,15 @@
+from brownie import accounts, network, config
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS=["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS=["mainnet-fork-dev"]
+
+def get_account_v2(index=None, id=None):
+    if (index):
+        return accounts[index]
+    if (id):
+        return accounts.load(id)
+    if (network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS 
+    or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
